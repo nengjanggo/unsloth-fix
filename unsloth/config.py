5baafd544c26da1c81bb3a5245f4999dd3a4aaa2
nengjanggo <sungdk0822@naver.com>
2025-11-24 15:11:53 +0000

    add UnslothGRPOTrainer.py

--- unsloth/config.py
+++ unsloth/config.py
@@ -1,7 +1,4 @@
 from datetime import datetime
-# model_name = 'Qwen/Qwen3-4B-Instruct-2507'
-# model_name = 'Qwen/Qwen3-1.7B-Base'
-# model_name = 'Qwen/Qwen2.5-Math-1.5B-Instruct'
 model_name = 'Qwen/Qwen2.5-3B-Instruct'
 max_length = 1024 * 3
 max_prompt_length = 256
@@ -12,9 +9,12 @@ output_dir = f'output/{model_name.split("/")[1]}/{time}'
 
 resume_from_checkpoint = False
 checkpoint_load_dir = ''
-wandb_project_name='huggingface'
+wandb_project_name=''
 wandb_run_name=''
 wandb_run_id=''
+
+if resume_from_checkpoint and checkpoint_load_dir:
+    output_dir = '/'.join(checkpoint_load_dir.split('/')[:-1])
 
 lora_rank = 16
 num_generations = 8
@@ -22,7 +22,7 @@ per_device_train_batch_size = 64
 gradient_accumulation_steps = 2
 learning_rate = 3e-5
 max_steps = 300
-save_steps=50
+save_steps=10
 report_to='wandb'
 # report_to='none'
 
@@ -32,8 +32,12 @@ eval_steps = 0.1 * max_steps
 eval_on_start = False
 
 execution_token_entropy_coef = 0.0
-advantage_schedule = 'none'
-# advantage_schedule = 'hicra'
+# advantage_schedule = 'none'
+advantage_schedule = 'hicra'
 # advantage_schedule = 'execution_token_entropy'
+execution_token_entropy_schedule_ceil = 0.4
+execution_token_entropy_schedule_ratio = 0.8
+initial_normalized_execution_token_entropy = 0.02
+
 
 gpu_memory_utilization = 0.3
